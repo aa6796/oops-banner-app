02aed5603eefab5c60d5c6221e64146fe75fcf13
Geethika <aa6796@srmist.edu.in>
2026-03-03 10:42:24 +0530

    Feature(uc5):print oops

---
+++ src/com.oppsbanner/UC6.java
@@ -0,0 +1,18 @@
+public class UC5{
+    public static void main(String[] args) {
+        String[] lines = {
+            String.join(" ", "   ***   ", "   ***   ", " ******   ", "  *****  "),
+            String.join(" ", " **   ** ", " **   ** ", " **    ** ", " **    **"),
+            String.join(" ", "**     **", "**     **", " **     **", "**       "),
+            String.join(" ", "**     **", "**     **", " **    ** ", "  ****   "),
+            String.join(" ", "**     **", "**     **", " ******   ", "      ** "),
+            String.join(" ", " **   ** ", " **   ** ", " **       ", "**     **"),
+            String.join(" ", "   ***   ", "   ***   ", " **       ", "  *****  ")
+        };
+
+        // Print using for-each loop
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
